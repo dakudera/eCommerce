5f6798ad55db2fe5ba88875659f3cd685909ecc1
dakudera <d.ponomarenkoa@gmail.com>
2024-10-29 17:39:48 +0200

    save

---
+++ frontend/src/app/config/app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '27vjCSFtgohSFJjJtfqXgwUBuj1akBJk',
+        issuer: 'https://dev-vbc4gptbpocwi2so.us.auth0.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scope: ['openid', 'profile', 'email']
+    }
+
+}
